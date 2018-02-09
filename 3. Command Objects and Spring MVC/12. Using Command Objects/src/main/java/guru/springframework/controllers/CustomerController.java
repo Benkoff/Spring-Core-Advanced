2a128272f9b0977fc48475ef935a300ffc203cdc
Ben Novikov <bigopendatanet@yahoo.com>
2018-02-09 15:23:55 +0200

    Add CustomerForm with converter

--- 3. Command Objects and Spring MVC/12. Using Command Objects/src/main/java/guru/springframework/controllers/CustomerController.java
+++ 3. Command Objects and Spring MVC/12. Using Command Objects/src/main/java/guru/springframework/controllers/CustomerController.java
@@ -1,5 +1,6 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.CustomerForm;
 import guru.springframework.domain.Customer;
 import guru.springframework.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,36 +27,54 @@ public class CustomerController {
     @RequestMapping({"/list", "/"})
     public String listCustomers(Model model){
         model.addAttribute("customers", customerService.listAll());
+
         return "customer/list";
     }
 
     @RequestMapping("/show/{id}")
     public String showCustomer(@PathVariable Integer id, Model model){
         model.addAttribute("customer", customerService.getById(id));
+
         return "customer/show";
     }
 
     @RequestMapping("/edit/{id}")
     public String edit(@PathVariable Integer id, Model model){
-        model.addAttribute("customer", customerService.getById(id));
+        Customer customer = customerService.getById(id);
+        CustomerForm customerForm = new CustomerForm();
+
+        customerForm.setCustomerId(customer.getId());
+        customerForm.setCustomerVersion(customer.getVersion());
+        customerForm.setEmail(customer.getEmail());
+        customerForm.setFirstName(customer.getFirstName());
+        customerForm.setLastName(customer.getLastName());
+        customerForm.setPhoneNumber(customer.getPhoneNumber());
+        customerForm.setUserId(customer.getUser().getId());
+        customerForm.setUserName(customer.getUser().getUsername());
+        customerForm.setUserVersion(customer.getUser().getVersion());
+        model.addAttribute("customer", customerForm);
+
         return "customer/customerform";
     }
 
     @RequestMapping("/new")
     public String newCustomer(Model model){
-        model.addAttribute("customer", new Customer());
+        model.addAttribute("customer", new CustomerForm());
+
         return "customer/customerform";
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String saveOrUpdate(Customer customer){
-        Customer newCustomer = customerService.saveOrUpdate(customer);
+    public String saveOrUpdate(CustomerForm customerForm){
+        Customer newCustomer = customerService.saveOrUpdateCustomerForm(customerForm);
+
         return "redirect:customer/show/" + newCustomer.getId();
     }
 
     @RequestMapping("/delete/{id}")
     public String delete(@PathVariable Integer id){
         customerService.delete(id);
+
         return "redirect:/customer/list";
     }
 }
--- 3. Command Objects and Spring MVC/12. Using Command Objects/src/main/java/guru/springframework/services/mapservices/CustomerServiceImpl.java
+++ 3. Command Objects and Spring MVC/12. Using Command Objects/src/main/java/guru/springframework/services/mapservices/CustomerServiceImpl.java
@@ -1,8 +1,11 @@
 package guru.springframework.services.mapservices;
 
+import guru.springframework.commands.CustomerForm;
+import guru.springframework.converters.CustomerFormToCustomer;
 import guru.springframework.domain.Customer;
 import guru.springframework.domain.DomainObject;
 import guru.springframework.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,13 @@ import java.util.List;
 @Service
 @Profile("map")
 public class CustomerServiceImpl extends AbstractMapService implements CustomerService {
+
+    private CustomerFormToCustomer customerFormToCustomer;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerFormToCustomer customerFormToCustomer) {
+        this.customerFormToCustomer = customerFormToCustomer;
+    }
 
     @Override
     public List<DomainObject> listAll() {
@@ -28,6 +38,19 @@ public class CustomerServiceImpl extends AbstractMapService implements CustomerS
     @Override
     public Customer saveOrUpdate(Customer domainObject) {
         return (Customer) super.saveOrUpdate(domainObject);
+    }
+
+    @Override
+    public Customer saveOrUpdateCustomerForm(CustomerForm customerForm) {
+        Customer newCustomer = customerFormToCustomer.convert(customerForm);
+
+        if(newCustomer.getUser().getId() != null){
+            Customer existingCustomer = getById(newCustomer.getId());
+
+            newCustomer.getUser().setEnabled(existingCustomer.getUser().getEnabled());
+        }
+
+        return saveOrUpdate(newCustomer);
     }
 
     @Override
